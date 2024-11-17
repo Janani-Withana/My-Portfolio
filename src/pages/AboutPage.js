fc3cc822d158a9ee1d70c57d2a33fcb92b434db2
Janani Jayasuriya Withana <99182048+Janani-Withana@users.noreply.github.com>
2024-11-17 15:33:27 +0530

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,7 +8,7 @@ import resumePDF1 from '../Assets/pdf/Withana J.W.J Resume ATS Format.pdf';
 
 const AboutPage = () => {
   const openURL = () => {
-    window.open(resumePDF, "_blank");
+    window.open(resumePDF1, "_blank");
   };
 
   return (
